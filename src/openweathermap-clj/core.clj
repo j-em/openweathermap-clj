225228819d3252d7ca1819fc291c9a8e3d09c764
j-em <jeremy@allard.io>
2016-08-12 23:48:02 -0400

    Add main implementation of function used to get current weather.

--- src/openweathermap-clj/core.clj
+++ src/openweathermap-clj/core.clj
@@ -1,2 +1,17 @@
-(ns openweathermap-clj.core)
+(ns openweathermap-clj.core
+  (:require [clj-http.client :as client]
+            [cemerick.url :as url]
+            [clojure.data.json :as json]
+            [clojure.walk :as walk]))
+
+(def api-link "http://api.openweathermap.org/data/2.5/")
+
+(defn current [key id]
+  (-> (url/url api-link "weather")
+      str
+      (client/get {:query-params {"q" id "appid" key} :as :json})
+      :body
+      json/read-str
+      walk/keywordize-keys))
+
 
